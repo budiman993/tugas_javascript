89b78cf8d2d6915fd06d1c11eab0229a139438e3
budiman993 <71246269+budiman993@users.noreply.github.com>
2020-09-18 10:38:12 +0700

    Add files via upload

---
+++ tugas1.js
@@ -0,0 +1,11 @@
+var namaDepan = "Budiman"
+const umur = 27
+
+function namaBelakang(){
+	let nama = "Abdurrohman"	
+	console.log(nama)
+}
+
+console.log(namaDepan)
+namaBelakang()
+console.log(umur)
